91eaff5fc184f75441e1e5f3db5c3e62fc42ff1b
SIVANANDANA R PILLAI <levelupsvr24@gmail.com>
2026-07-21 00:38:03 +0530

    Time: 8 ms (53.16%), Space: 19.3 MB (18.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s=str(x)
-        if s==s[::-1]:
-            return True
-        else:
+        if x<0:
             return False
+        og=x
+        rev=0
+        while x>0:
+            digit=x%10
+            rev=rev*10+digit
+            x//=10
+        return og==rev
 
         
